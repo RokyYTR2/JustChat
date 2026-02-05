d0c850aeb5fbe527ba88010def24592b78e1284d
rokyytr.official <163911146+RokyYTR2@users.noreply.github.com>
2026-02-05 17:27:39 +0100

    0.0.5

--- src/main/java/dev/meyba/justChat/commands/JustChatCommand.java
+++ src/main/java/dev/meyba/justChat/commands/JustChatCommand.java
@@ -1,12 +1,15 @@
 package dev.meyba.justChat.commands;
 
 import dev.meyba.justChat.JustChat;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class JustChatCommand implements CommandExecutor, TabCompleter {
@@ -49,6 +52,27 @@ public class JustChatCommand implements CommandExecutor, TabCompleter {
                 plugin.getServer().broadcastMessage(plugin.getChatUtils().formatMessage(message));
                 return true;
 
+            case "msg":
+                return forwardCommand(sender, "msg", args);
+
+            case "reply":
+                return forwardCommand(sender, "reply", args);
+
+            case "mute":
+                return forwardCommand(sender, "mute", args);
+
+            case "unmute":
+                return forwardCommand(sender, "unmute", args);
+
+            case "ignore":
+                return forwardCommand(sender, "ignore", args);
+
+            case "unignore":
+                return forwardCommand(sender, "unignore", args);
+
+            case "clearchat":
+                return forwardCommand(sender, "clearchat", args);
+
             case "help":
             default:
                 sendHelp(sender);
@@ -59,33 +83,41 @@ public class JustChatCommand implements CommandExecutor, TabCompleter {
     private void sendHelp(CommandSender sender) {
         String version = plugin.getDescription().getVersion();
 
-        sender.sendMessage(plugin.getChatUtils().translateColorCodes("&#A855F7&l━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"));
-        sender.sendMessage(plugin.getChatUtils().translateColorCodes("     &#A855F7&lᴊ&#AA5BF7&lᴜ&#AF61F7&lꜱ&#B467F7&lᴛ&#B96DF7&lᴄ&#BE73F7&lʜ&#C379F7&lᴀ&#C87FF7&lᴛ   &#FFFFFFᴄᴏᴍᴍᴀɴᴅꜱ"));
-        sender.sendMessage(plugin.getChatUtils().translateColorCodes("&#A855F7&l━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"));
+        sender.sendMessage(plugin.getChatUtils().translateColorCodes("&#CFBC4D&l━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"));
+        sender.sendMessage(plugin.getChatUtils().translateColorCodes("     &#CFBC4D&lᴊ&#D1BD4D&lᴜ&#D2BD4C&lꜱ&#D4BE4C&lᴛ&#D6BE4B&lᴄ&#D8BF4B&lʜ&#D9BF4A&lᴀ&#DBC04A&lᴛ   &#FFFFFFᴄᴏᴍᴍᴀɴᴅꜱ"));
+        sender.sendMessage(plugin.getChatUtils().translateColorCodes("&#CFBC4D&l━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"));
         sender.sendMessage("");
 
-        sender.sendMessage(plugin.getChatUtils().translateColorCodes(" &#A855F7&l● &#FFFFFFᴄᴏʀᴇ"));
-        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#C084FC/ᴄʜᴀᴛ ʀᴇʟᴏᴀᴅ &#8E8E8E- &#7F7F7Fʀᴇʟᴏᴀᴅ ᴄᴏɴꜰɪɢᴜʀᴀᴛɪᴏɴ"));
-        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#C084FC/ᴄʜᴀᴛ ʜᴇʟᴘ &#8E8E8E- &#7F7F7Fꜱʜᴏᴡ ᴛʜɪꜱ ᴍᴇɴᴜ"));
+        sender.sendMessage(plugin.getChatUtils().translateColorCodes(" &#CFBC4D&l● &#FFFFFFᴄᴏʀᴇ"));
+        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#DBC04A/ᴄʜᴀᴛ ʀᴇʟᴏᴀᴅ &#8E8E8E- &#7F7F7Fʀᴇʟᴏᴀᴅ ᴄᴏɴꜰɪɢᴜʀᴀᴛɪᴏɴ"));
+        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#DBC04A/ᴄʜᴀᴛ ʜᴇʟᴘ &#8E8E8E- &#7F7F7Fꜱʜᴏᴡ ᴛʜɪꜱ ᴍᴇɴᴜ"));
         sender.sendMessage("");
 
-        sender.sendMessage(plugin.getChatUtils().translateColorCodes(" &#A855F7&l● &#FFFFFFᴄʜᴀᴛ"));
-        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#C084FC/ᴍꜱɢ <ᴘʟᴀʏᴇʀ> <ᴍᴇꜱꜱᴀɢᴇ> &#8E8E8E- &#7F7F7Fꜱᴇɴᴅ ᴘʀɪᴠᴀᴛᴇ ᴍᴇꜱꜱᴀɢᴇ"));
-        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#C084FC/ʀᴇᴘʟʏ <ᴍᴇꜱꜱᴀɢᴇ> &#8E8E8E- &#7F7F7Fʀᴇᴘʟʏ ᴛᴏ ʟᴀꜱᴛ ᴍᴇꜱꜱᴀɢᴇ"));
-        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#C084FC/ɪɢɴᴏʀᴇ <ᴘʟᴀʏᴇʀ> &#8E8E8E- &#7F7F7Fɪɢɴᴏʀᴇ ᴘʟᴀʏᴇʀ"));
-        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#C084FC/ᴜɴɪɢɴᴏʀᴇ <ᴘʟᴀʏᴇʀ> &#8E8E8E- &#7F7F7Fᴜɴɪɢɴᴏʀᴇ ᴘʟᴀʏᴇʀ"));
+        sender.sendMessage(plugin.getChatUtils().translateColorCodes(" &#CFBC4D&l● &#FFFFFFᴄʜᴀᴛ"));
+        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#DBC04A/ᴍꜱɢ <ᴘʟᴀʏᴇʀ> <ᴍᴇꜱꜱᴀɢᴇ> &#8E8E8E- &#7F7F7Fꜱᴇɴᴅ ᴘʀɪᴠᴀᴛᴇ ᴍᴇꜱꜱᴀɢᴇ"));
+        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#DBC04A/ʀᴇᴘʟʏ <ᴍᴇꜱꜱᴀɢᴇ> &#8E8E8E- &#7F7F7Fʀᴇᴘʟʏ ᴛᴏ ʟᴀꜱᴛ ᴍᴇꜱꜱᴀɢᴇ"));
+        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#DBC04A/ɪɢɴᴏʀᴇ <ᴘʟᴀʏᴇʀ> &#8E8E8E- &#7F7F7Fɪɢɴᴏʀᴇ ᴘʟᴀʏᴇʀ"));
+        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#DBC04A/ᴜɴɪɢɴᴏʀᴇ <ᴘʟᴀʏᴇʀ> &#8E8E8E- &#7F7F7Fᴜɴɪɢɴᴏʀᴇ ᴘʟᴀʏᴇʀ"));
         sender.sendMessage("");
 
-        sender.sendMessage(plugin.getChatUtils().translateColorCodes(" &#A855F7&l● &#FFFFFFᴍᴏᴅᴇʀᴀᴛɪᴏɴ"));
-        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#C084FC/ᴍᴜᴛᴇ <ᴘʟᴀʏᴇʀ> [ᴛɪᴍᴇ] &#8E8E8E- &#7F7F7Fᴍᴜᴛᴇ ᴘʟᴀʏᴇʀ"));
-        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#C084FC/ᴜɴᴍᴜᴛᴇ <ᴘʟᴀʏᴇʀ> &#8E8E8E- &#7F7F7Fᴜɴᴍᴜᴛᴇ ᴘʟᴀʏᴇʀ"));
-        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#C084FC/ᴄʜᴀᴛ ᴍᴜᴛᴇᴄʜᴀᴛ &#8E8E8E- &#7F7F7Fᴛᴏɢɢʟᴇ ɢʟᴏʙᴀʟ ᴄʜᴀᴛ"));
-        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#C084FC/ᴄʟᴇᴀʀᴄʜᴀᴛ &#8E8E8E- &#7F7F7Fᴄʟᴇᴀʀ ᴄʜᴀᴛ"));
+        sender.sendMessage(plugin.getChatUtils().translateColorCodes(" &#CFBC4D&l● &#FFFFFFᴍᴏᴅᴇʀᴀᴛɪᴏɴ"));
+        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#DBC04A/ᴍᴜᴛᴇ <ᴘʟᴀʏᴇʀ> [ᴛɪᴍᴇ] &#8E8E8E- &#7F7F7Fᴍᴜᴛᴇ ᴘʟᴀʏᴇʀ"));
+        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#DBC04A/ᴜɴᴍᴜᴛᴇ <ᴘʟᴀʏᴇʀ> &#8E8E8E- &#7F7F7Fᴜɴᴍᴜᴛᴇ ᴘʟᴀʏᴇʀ"));
+        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#DBC04A/ᴄʜᴀᴛ ᴍᴜᴛᴇᴄʜᴀᴛ &#8E8E8E- &#7F7F7Fᴛᴏɢɢʟᴇ ɢʟᴏʙᴀʟ ᴄʜᴀᴛ"));
+        sender.sendMessage(plugin.getChatUtils().translateColorCodes("   &#DBC04A/ᴄʟᴇᴀʀᴄʜᴀᴛ &#8E8E8E- &#7F7F7Fᴄʟᴇᴀʀ ᴄʜᴀᴛ"));
         sender.sendMessage("");
 
-        sender.sendMessage(plugin.getChatUtils().translateColorCodes(" &#8E8E8Eᴠᴇʀꜱɪᴏɴ: &#4BC0F1" + version + " &#8E8E8E| &#FFFFFFᴍᴀᴅᴇ ᴡɪᴛʜ &#FF6B6B❤ &#FFFFFFʙʏ &#4BC0F1ᴍᴇʏʙᴀ"));
-        sender.sendMessage(plugin.getChatUtils().translateColorCodes("&#A855F7&l━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"));
+        sender.sendMessage(plugin.getChatUtils().translateColorCodes(" &#8E8E8Eᴠᴇʀꜱɪᴏɴ: &#DBC04A" + version));
+        sender.sendMessage(plugin.getChatUtils().translateColorCodes("&#CFBC4D&l━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"));
         sender.sendMessage("");
+    }
+
+    private boolean forwardCommand(CommandSender sender, String commandName, String[] args) {
+        if (args.length <= 1) {
+            return Bukkit.dispatchCommand(sender, commandName);
+        }
+        String forwardArgs = String.join(" ", Arrays.copyOfRange(args, 1, args.length));
+        return Bukkit.dispatchCommand(sender, commandName + " " + forwardArgs);
     }
 
     @Override
@@ -97,6 +129,25 @@ public class JustChatCommand implements CommandExecutor, TabCompleter {
             if (sender.hasPermission("justchat.reload")) suggestions.add("reload");
             if (sender.hasPermission("justchat.help")) suggestions.add("help");
             if (sender.hasPermission("justchat.mutechat")) suggestions.add("mutechat");
+            if (sender.hasPermission("justchat.msg")) suggestions.add("msg");
+            if (sender.hasPermission("justchat.reply")) suggestions.add("reply");
+            if (sender.hasPermission("justchat.mute")) suggestions.add("mute");
+            if (sender.hasPermission("justchat.unmute")) suggestions.add("unmute");
+            if (sender.hasPermission("justchat.ignore")) suggestions.add("ignore");
+            if (sender.hasPermission("justchat.ignore")) suggestions.add("unignore");
+            if (sender.hasPermission("justchat.clearchat")) suggestions.add("clearchat");
+        } else if (args.length == 2) {
+            String sub = args[0].toLowerCase();
+            if ((sub.equals("msg") && sender.hasPermission("justchat.msg")) ||
+                (sub.equals("mute") && sender.hasPermission("justchat.mute")) ||
+                (sub.equals("unmute") && sender.hasPermission("justchat.unmute")) ||
+                (sub.equals("ignore") && sender.hasPermission("justchat.ignore")) ||
+                (sub.equals("unignore") && sender.hasPermission("justchat.ignore"))) {
+                return Bukkit.getOnlinePlayers().stream()
+                        .map(Player::getName)
+                        .filter(name -> name.toLowerCase().startsWith(args[1].toLowerCase()))
+                        .toList();
+            }
         }
 
         for (String s : suggestions) {
